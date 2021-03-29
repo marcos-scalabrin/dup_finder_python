534d7f1bbe0253ecb662faf40fdc29cdba03d47e
marcos-scalabrin <marcos.scalabrin@gmail.com>
2021-03-29 01:05:18 -0300

    nome do arquivo principal renomeado

--- test/tests.py
+++ test/tests.py
@@ -1,12 +1,9 @@
 import os,sys,inspect
-print(os.getcwd())
+# inspect garante que caminho será referente à esse arquivo e não de onde foi
+# rodado o python. 
+# premissa é que códigos (/src) e testes (/test) estã no mesmo nivel
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-print(os.path.abspath('.')+'\n\n')
-print(currentdir)
-print('\n\n')
 parentdir = os.path.dirname(currentdir)
-print(sys.path)
-print('\n\n')
 sys.path.insert(0,parentdir+'/src') 
-print(sys.path)
 
+import 
